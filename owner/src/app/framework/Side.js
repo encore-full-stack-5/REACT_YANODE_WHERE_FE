1bafe8e37e01e7927d26dea13833aebd1a9808f6
ParkMiram <myurm@naver.com>
2024-04-03 16:31:19 +0900

    [가게정보, 메뉴등록, 메뉴상세팝업] 추가

--- owner/src/app/framework/Side.js
+++ owner/src/app/framework/Side.js
@@ -7,22 +7,22 @@ export default function Side() {
                 {/* 로고 이미지 넣기 */}
                 <p className={framework.logo}><span>W</span>here?</p>
 
-                {/* 프로필 -> Header로 이동 */}
-                {/*<div className={framework.userInfo}>*/}
-                {/*    <p className={framework.userNm}><b>야노드</b>님</p>*/}
-                {/*    <p className={framework.userId}>yanode00</p>*/}
-                {/*</div>*/}
+                <div className={framework.userInfo}>
+                    <p className={framework.userNm}><b>야노드</b>님</p>
+                    <p className={framework.userId}>yanode00</p>
+                </div>
 
                 {/* 메뉴 */}
                 <ul className={framework.sideList}>
-                    <li className={[framework.menu, framework.on].join(' ')}>가게 정보</li>
-                    <li className={framework.menu}>메뉴 등록</li>
-                    <li className={framework.menu}>판매 내역</li>
-                    <li className={framework.menu}>주문 접수</li>
-                    <li className={framework.menu}>내 정보</li>
+                    <li className={framework.side}>대시보드</li>
+                    <li className={[framework.side].join(' ')}>가게 정보</li>
+                    <li className={[framework.side, framework.on].join(' ')}>메뉴 등록</li>
+                    <li className={framework.side}>판매 내역</li>
+                    <li className={framework.side}>주문 접수</li>
+                    <li className={framework.side}>내 정보</li>
                 </ul>
                 <div className={framework.logoutWrap}>
-                    <button className={framework.logout}>
+                <button className={framework.logout}>
                         {/* 로그아웃 이미지 자리 */}
                         <p>Log out</p>
                     </button>
