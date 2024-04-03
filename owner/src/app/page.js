97f88d635c8e2e60e54bf999a0fe50f29f8788dd
Sujin1517 <11tnwls12@gmail.com>
2024-04-03 20:24:46 +0900

    using axios

--- owner/src/app/page.js
+++ owner/src/app/page.js
@@ -1,16 +1,30 @@
 "use client"
 
+import axios from 'axios';
 import {useRouter} from "next/navigation";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function Home() {
-  const router = useRouter();
+  const [list, setList] = useState();
+  // const router = useRouter();
 
+  const getdata = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/owners/stores/2");
+      setList(response.data[0]);
+      console.log("킹미람");
+    } catch (error) {
+      alert('조회 에러');
+    }
+  }
+  
   useEffect(() => {
-    router.push("/menu");
+    getdata();
   }, []);
+
   return (
       <>
+        <p>SHOP_ID: {list?.SHOP_ID}</p>
       </>
   )
 }
