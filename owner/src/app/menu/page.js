5db467cb237ea5273e9d4e045a13def3ced23052
tjdaniel1 <robot2daniel@gmail.com>
2024-04-04 17:48:46 +0900

    menu page update undone

--- owner/src/app/menu/page.js
+++ owner/src/app/menu/page.js
@@ -1,109 +1,68 @@
+"use client";
+
+import axios from "axios";
 import common from "@/resources/common.module.css";
 import Image from "next/image";
+import Input from "@/app/component/Input";
+import Textarea from "@/app/component/Textarea";
+import { useEffect, useState } from "react";
 
 export default function menu() {
-    return (
-        <>
-            <div>
-                <div className={common.pageTitleWrap}>
-                    <h2>메뉴 등록</h2>
-                    <button className={common.pageBtn}>추가</button>
-                </div>
-                {/* 필터 */}
-                <div className={common.menuFilter}>
-                    <button className={common.on}>전체</button>
-                    <button>품절 메뉴</button>
-                    <button>숨긴 메뉴</button>
-                </div>
-                {/* 리스트 */}
-                <ul className={common.menuList}>
-                    <li className={common.menu}>
-                        <div className={common.menuImg}>
-                            {/*<Image src={} alt={}/>*/}
-                            <span>메뉴 사진</span>
-                        </div>
-                        <p className={common.menuNm}>평양 물냉면</p>
-                        <p className={common.menuDesc}>아주아주 맛있음</p>
-                        <div className={common.menuOpt}>
-                            <p>옵션 개수</p>
-                            <p>2</p>
-                        </div>
-                        <div className={common.menuYn}>
-                            <p>메뉴 상태</p>
-                            <p>판매</p>
-                        </div>
-                        <p className={common.menuPrc}>18,000</p>
-                    </li>
-                    <li className={common.menu}>
-                        <div className={common.menuImg}>
-                            {/*<Image src={} alt={}/>*/}
-                            <span>메뉴 사진</span>
-                        </div>
-                        <p className={common.menuNm}>함흥 비빔냉면</p>
-                        <p className={common.menuDesc}>함흥은 비냉이지~</p>
-                        <div className={common.menuOpt}>
-                            <p>옵션 개수</p>
-                            <p>5</p>
-                        </div>
-                        <div className={common.menuYn}>
-                            <p>메뉴 상태</p>
-                            <p>품절</p>
-                        </div>
-                        <p className={common.menuPrc}>18,000</p>
-                    </li>
-                    <li className={common.menu}>
-                        <div className={common.menuImg}>
-                            {/*<Image src={} alt={}/>*/}
-                            <span>메뉴 사진</span>
-                        </div>
-                        <p className={common.menuNm}>닭강정</p>
-                        <p className={common.menuDesc}>닭강정 맛있어</p>
-                        <div className={common.menuOpt}>
-                            <p>옵션 개수</p>
-                            <p>10</p>
-                        </div>
-                        <div className={common.menuYn}>
-                            <p>메뉴 상태</p>
-                            <p>숨김</p>
-                        </div>
-                        <p className={common.menuPrc}>20,000</p>
-                    </li>
-                    <li className={common.menu}>
-                        <div className={common.menuImg}>
-                            {/*<Image src={} alt={}/>*/}
-                            <span>메뉴 사진</span>
-                        </div>
-                        <p className={common.menuNm}>헤이즐넛 커피</p>
-                        <p className={common.menuDesc}>매머드에서 공수해 온 헤이즐넛 커피이다.</p>
-                        <div className={common.menuOpt}>
-                            <p>옵션 개수</p>
-                            <p>15</p>
-                        </div>
-                        <div className={common.menuYn}>
-                            <p>메뉴 상태</p>
-                            <p>판매</p>
-                        </div>
-                        <p className={common.menuPrc}>2,400</p>
-                    </li>
-                    <li className={common.menu}>
-                        <div className={common.menuImg}>
-                            {/*<Image src={} alt={}/>*/}
-                            <span>메뉴 사진</span>
-                        </div>
-                        <p className={common.menuNm}>대국민 미술작가 오디션 MBN 화100</p>
-                        <p className={common.menuDesc}>나도 화100?! 이벤트 QR코드 찍고 컵아트 화가 데뷔? 3월 23일부터 매주 토요일 17시</p>
-                        <div className={common.menuOpt}>
-                            <p>옵션 개수</p>
-                            <p>15</p>
-                        </div>
-                        <div className={common.menuYn}>
-                            <p>메뉴 상태</p>
-                            <p>판매</p>
-                        </div>
-                        <p className={common.menuPrc}>2,400</p>
-                    </li>
-                </ul>
+  // state
+  const [data, setData] = useState([]);
+
+  // function
+  const getdata = async () => {
+    try {
+      const response = await axios.get(
+        "http://192.168.80.39:3001/owners/products/31"
+      );
+      setData(response.data);
+    } catch (error) {
+      alert("조회 에러");
+    }
+  };
+
+  // useEffect
+  useEffect(() => {
+    getdata();
+  }, []);
+  return (
+    <>
+      {console.log(data)}
+      <div>
+        <div className={common.pageTitleWrap}>
+          <h2>메뉴 등록</h2>
+          <button className={common.pageBtn}>추가</button>
+        </div>
+        {/* 필터 */}
+        <div className={common.menuFilter}>
+          <button className={common.on}>전체</button>
+          <button>품절 메뉴</button>
+          <button>숨긴 메뉴</button>
+        </div>
+        {/* 리스트 */}
+        <ul className={common.menuList}>
+          {}
+          <li className={common.menu}>
+            <div className={common.menuImg}>
+              {/*<Image src={} alt={}/>*/}
+              <span>메뉴 사진</span>
             </div>
-        </>
-    )
+            <p className={common.menuNm}>평양 물냉면</p>
+            <p className={common.menuDesc}>아주아주 맛있음</p>
+            <div className={common.menuOpt}>
+              <p>옵션 개수</p>
+              <p>2</p>
+            </div>
+            <div className={common.menuYn}>
+              <p>메뉴 상태</p>
+              <p>판매</p>
+            </div>
+            <p className={common.menuPrc}>18,000</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
--- owner/src/app/store/page.js
+++ owner/src/app/store/page.js
@@ -1,86 +1,92 @@
-"use client"
+"use client";
 
-import axios from 'axios';
-import common from '@/resources/common.module.css';
+import axios from "axios";
+import common from "@/resources/common.module.css";
 import Image from "next/image";
 import Input from "@/app/component/Input";
 import Textarea from "@/app/component/Textarea";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 export default function store() {
-    // state
-    const [data, setData] = useState();
+  // state
+  const [data, setData] = useState();
 
-    // function
-    const getdata = async () => {
-        try {
-            const response = await axios.get("http://192.168.80.39:3001/owners/stores/24");
-            setData(response.data[0]);
-        } catch (error) {
-            alert('조회 에러');
-        }
+  // function
+  const getdata = async () => {
+    try {
+      const response = await axios.get(
+        "http://192.168.80.39:3001/owners/stores/24"
+      );
+      setData(response.data[0]);
+    } catch (error) {
+      alert("조회 에러");
     }
+  };
 
-    // useEffect
-    useEffect(() => {
-        getdata();
-      }, []);
+  // useEffect
+  useEffect(() => {
+    getdata();
+  }, []);
 
-    return (
-        <>
-            {console.log(data)}
-            <div>
-                <div className={common.pageTitleWrap}>
-                    <h2>가게 정보</h2>
-                    <button className={common.pageBtn}>저장</button>
-                </div>
-                <div className={common.storeContent}>
-                    <div className={common.storeLayout}>
-                        <div className={common.storeImg}>
-                            {/*<input type="file"/>*/}
-                            <Image src={''} alt={'가게 사진'} />
-                        </div>
-                        <div className={common.storeInptWrap}>
-                            <Input
-                                name={"업종"}
-                                type={"text"}
-                                value={data ? (data.CTGRY) : ""}
-                            />
-                            <div className={common.inptWrap}>
-                                <label>위치</label>
-                                <input
-                                    type="text"
-                                    className={common.inpt}
-                                    placeholder={"기본 주소"}
-                                />
-                                <input
-                                    type="text"
-                                    className={common.inpt}
-                                    placeholder={"상세 주소"}
-                                />
-                            </div>
-                            <Input name={"가게 이름"} type={"type"} />
-                        </div>
-                    </div>
-                    <div className={common.storeLayout}>
-                        <div className={common.storeInptWrap}>
-                            <Textarea name={"가게 설명"} />
-                        </div>
-                    </div>
-                    <div className={common.storeLayout}>
-                        <div className={common.storeInptWrap}>
-                            <div className={common.setTime}>
-                                <Input name={"OPEN"} type={"time"}/>
-                                <span>~</span>
-                                <Input name={"CLOSE"} type={"time"}/>
-                            </div>
-                            <div className={common.inptWrap}>
-                                <Input name={"전화번호"} type={"number"} placeholder={"숫자만 입력"}/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      {console.log(data)}
+      <div>
+        <div className={common.pageTitleWrap}>
+          <h2>가게 정보</h2>
+          <button className={common.pageBtn}>저장</button>
+        </div>
+        <div className={common.storeContent}>
+          <div className={common.storeLayout}>
+            <div className={common.storeImg}>
+              {/*<input type="file"/>*/}
+              <Image src={""} alt={"가게 사진"} />
             </div>
-        </>
-    )
+            <div className={common.storeInptWrap}>
+              <Input
+                name={"업종"}
+                type={"text"}
+                value={data ? data.CTGRY : ""}
+              />
+              <div className={common.inptWrap}>
+                <label>위치</label>
+                <input
+                  type="text"
+                  className={common.inpt}
+                  placeholder={"기본 주소"}
+                />
+                <input
+                  type="text"
+                  className={common.inpt}
+                  placeholder={"상세 주소"}
+                />
+              </div>
+              <Input name={"가게 이름"} type={"type"} />
+            </div>
+          </div>
+          <div className={common.storeLayout}>
+            <div className={common.storeInptWrap}>
+              <Textarea name={"가게 설명"} />
+            </div>
+          </div>
+          <div className={common.storeLayout}>
+            <div className={common.storeInptWrap}>
+              <div className={common.setTime}>
+                <Input name={"OPEN"} type={"time"} />
+                <span>~</span>
+                <Input name={"CLOSE"} type={"time"} />
+              </div>
+              <div className={common.inptWrap}>
+                <Input
+                  name={"전화번호"}
+                  type={"number"}
+                  placeholder={"숫자만 입력"}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
