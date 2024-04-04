d9a09153a5eb7c26e442c9b2a634634c765f0b31
ParkMiram <myurm@naver.com>
2024-04-04 12:30:10 +0900

    [매출조회] 화면 추가

--- owner/src/app/sale/page.js
+++ owner/src/app/sale/page.js
@@ -1,7 +1,164 @@
+import common from "@/resources/common.module.css";
+
 export default function sale() {
     return (
         <>
-
+            <div className={common.saleContainer}>
+                <div className={common.pageTitleWrap}>
+                    <h2>매출 조회</h2>
+                </div>
+                <div className={common.saleSrchContainer}>
+                    <div className={common.saleSrchWrap}>
+                        <div className={common.srchListWrap}>
+                            <ul className={common.srchList}>
+                                <li className={[common.srch, common.full].join(' ')}>
+                                    <div className={common.inptWrap}>
+                                        <label>날짜</label>
+                                        <div className={common.srchDate}>
+                                            <input
+                                                type="date"
+                                                className={common.inpt}
+                                            />
+                                            <span>~</span>
+                                            <input
+                                                type="date"
+                                                className={common.inpt}
+                                            />
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <button className={common.inq}>조회</button>
+                        </div>
+                        <div className={common.historyWrap}>
+                            <div className={common.history}>
+                                <p className={common.historyNm}>주문 수</p>
+                                <p className={common.historyVal}>452</p>
+                            </div>
+                            <div className={common.history}>
+                                <p className={common.historyNm}>판매 수량</p>
+                                <p className={common.historyVal}>601</p>
+                            </div>
+                            <div className={common.history}>
+                                <p className={common.historyNm}>전체 판매가</p>
+                                <p className={common.historyVal}>10,058,600</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={common.saleSrchWrap}>
+                        <div className={common.srchListWrap}>
+                            <ul className={common.srchList}>
+                                <li className={[common.srch, common.full].join(' ')}>
+                                    <div className={common.inptWrap}>
+                                        <label>상품</label>
+                                        <input
+                                            type="text"
+                                            className={common.inpt}
+                                        />
+                                    </div>
+                                </li>
+                            </ul>
+                            <button className={common.inq}>조회</button>
+                        </div>
+                        <div className={common.historyWrap}>
+                            <div className={common.saleTableWrap}>
+                                <div className={common.saleTableHead}>
+                                    <p>상품명</p>
+                                    <p>판매 수</p>
+                                </div>
+                                <ul className={common.saleTable}>
+                                    <li className={common.saleList}>
+                                        {/*상품명*/}
+                                        <p>평양 물냉면</p>
+                                        {/*상품명*/}
+                                        <p>207</p>
+                                    </li>
+                                    <li className={common.saleList}>
+                                        {/*상품명*/}
+                                        <p>평양 물냉면</p>
+                                        {/*상품명*/}
+                                        <p>207</p>
+                                    </li>
+                                    <li className={common.saleList}>
+                                        {/*상품명*/}
+                                        <p>평양 물냉면</p>
+                                        {/*상품명*/}
+                                        <p>207</p>
+                                    </li>
+                                    <li className={common.saleList}>
+                                        {/*상품명*/}
+                                        <p>평양 물냉면</p>
+                                        {/*상품명*/}
+                                        <p>207</p>
+                                    </li>
+                                    <li className={common.saleList}>
+                                        {/*상품명*/}
+                                        <p>평양 물냉면</p>
+                                        {/*상품명*/}
+                                        <p>207</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={common.historyContainer}>
+                    <h3>매출 내역</h3>
+                    <div className={common.saleHistoryWrap}>
+                        <div className={common.saleHistory}>
+                            <p>오늘</p>
+                            <div className={common.historyWrap}>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>주문 수</p>
+                                    <p className={common.historyVal}>452</p>
+                                </div>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>판매 수량</p>
+                                    <p className={common.historyVal}>601</p>
+                                </div>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>전체 판매가</p>
+                                    <p className={common.historyVal}>10,058,600</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={common.saleHistory}>
+                            <p>당월</p>
+                            <div className={common.historyWrap}>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>주문 수</p>
+                                    <p className={common.historyVal}>452</p>
+                                </div>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>판매 수량</p>
+                                    <p className={common.historyVal}>601</p>
+                                </div>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>전체 판매가</p>
+                                    <p className={common.historyVal}>10,058,600</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={common.saleHistory}>
+                            <p>올해</p>
+                            <div className={common.historyWrap}>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>주문 수</p>
+                                    <p className={common.historyVal}>452</p>
+                                </div>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>판매 수량</p>
+                                    <p className={common.historyVal}>601</p>
+                                </div>
+                                <div className={common.history}>
+                                    <p className={common.historyNm}>전체 판매가</p>
+                                    <p className={common.historyVal}>10,058,600</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
