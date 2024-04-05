6631cdc8ed671e9d9070a3f213122d90f43377d9
heeseo333 <heeseo310@gmail.com>
2024-04-05 21:10:27 +0900

    내정보

--- owner/src/app/framework/Side.js
+++ owner/src/app/framework/Side.js
@@ -1,63 +1,80 @@
-'use client'
+"use client";
 
-import framework from '@/resources/framework.module.css';
-import {useRouter} from "next/navigation";
-import {useState} from "react";
+import framework from "@/resources/framework.module.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Side() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <>
-            <div className={framework.sideWrap}>
-                <div>
-                    {/* 로고 이미지 넣기 */}
-                    <p className={framework.logo}><span>W</span>here?</p>
-                    <div className={framework.userInfo}>
-                        <p className={framework.userNm}><b>야노드</b>님</p>
-                        <p className={framework.userId}>yanode00</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className={framework.sideWrap}>
+        <div>
+          {/* 로고 이미지 넣기 */}
+          <p className={framework.logo}>
+            <span>W</span>here?
+          </p>
+          <div className={framework.userInfo}>
+            <p className={framework.userNm}>
+              <b>야노드</b>님
+            </p>
+            <p className={framework.userId}>yanode00</p>
+          </div>
+        </div>
 
-                {/* 메뉴 */}
-                <ul className={framework.sideList}>
-                    <li
-                        className={[framework.side, framework.on].join(' ')}
-                        onClick={() => router.push("/")}
-                        id={"dashboard"}
-                    >대시보드</li>
-                    <li
-                        className={[framework.side].join(' ')}
-                        onClick={() => router.push("/store")}
-                        id={"store"}
-                    >가게 정보</li>
-                    <li
-                        className={[framework.side].join(' ')}
-                        onClick={() => router.push("/menu")}
-                        id={"menu"}
-                    >메뉴 등록</li>
-                    <li
-                        className={[framework.side].join(' ')}
-                        onClick={() => router.push("/ord")}
-                        id={"ord"}
-                    >주문 접수<span>3</span></li>
-                    <li
-                        className={[framework.side].join(' ')}
-                        onClick={() => router.push("/sale")}
-                        id={"sale"}
-                    >매출 조회</li>
-                    <li
-                        className={framework.side}
-                        id={"name"}
-                    >내 정보</li>
-                </ul>
-                <div className={framework.logoutWrap}>
-                <button className={framework.logout}>
-                        {/* 로그아웃 이미지 자리 */}
-                        <p>Log out</p>
-                    </button>
-                </div>
-            </div>
-        </>
-    )
+        {/* 메뉴 */}
+        <ul className={framework.sideList}>
+          <li
+            className={[framework.side, framework.on].join(" ")}
+            onClick={() => router.push("/")}
+            id={"dashboard"}
+          >
+            대시보드
+          </li>
+          <li
+            className={[framework.side].join(" ")}
+            onClick={() => router.push("/store")}
+            id={"store"}
+          >
+            가게 정보
+          </li>
+          <li
+            className={[framework.side].join(" ")}
+            onClick={() => router.push("/menu")}
+            id={"menu"}
+          >
+            메뉴 등록
+          </li>
+          <li
+            className={[framework.side].join(" ")}
+            onClick={() => router.push("/ord")}
+            id={"ord"}
+          >
+            주문 접수<span>3</span>
+          </li>
+          <li
+            className={[framework.side].join(" ")}
+            onClick={() => router.push("/sale")}
+            id={"sale"}
+          >
+            매출 조회
+          </li>
+          <li
+            className={[framework.side].join(" ")}
+            onClick={() => router.push("/my")}
+            id={"name"}
+          >
+            내 정보
+          </li>
+        </ul>
+        <div className={framework.logoutWrap}>
+          <button className={framework.logout}>
+            {/* 로그아웃 이미지 자리 */}
+            <p>Log out</p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
--- owner/src/app/my/page.js
+++ owner/src/app/my/page.js
@@ -1,7 +1,79 @@
-export default function my() {
-    return (
-        <>
+"use client";
+import axios from "axios";
+import common from "@/resources/common.module.css";
+import Input from "@/app/component/Input";
+import Image from "next/image";
+import "/front-end/react_yanode_where_FE/owner/src/resources/my.css";
 
-        </>
-    )
+export default function my() {
+  return (
+    <>
+      <div className={common.pageTitleWrap}>
+        <h2>가게 정보</h2>
+        <button className={common.pageBtn}>수정</button>
+      </div>
+
+      <div
+        className={common.storeContent}
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexDirection: "column",
+          marginTop: 0,
+        }}
+      >
+        <div
+          className={common.storeLayout}
+          style={{ alignItems: "first baseline", justifyContent: "center" }}
+        >
+          <div className={common.storeImg}>
+            {/*<input type="file"/>*/}
+            <Image src={""} alt={"가게 사진"} />
+          </div>
+          <div
+            className={common.storeInptWrap}
+            style={{ gap: "30px", width: "400px" }}
+          >
+            <input
+              type="text"
+              className={common.inpt}
+              placeholder="이름"
+              //   style={{
+              //     backgroundColor: "",
+              //     ,
+              //     ,
+              //     ,,
+
+              //   }}
+            ></input>
+            <input
+              type="text"
+              className={common.inpt}
+              placeholder="이메일"
+            ></input>
+            <input
+              type="text"
+              className={common.inpt}
+              placeholder="비밀번호"
+            ></input>
+            <input
+              type="text"
+              className={common.inpt}
+              placeholder="전화번호"
+            ></input>
+            <input
+              type="text"
+              className={common.inpt}
+              placeholder={"기본 주소"}
+            />
+            <input
+              type="text"
+              className={common.inpt}
+              placeholder={"상세 주소"}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
