b2f4d20704aa725bb2f2069b7b4dd57f1f8af91c
heeseo333 <heeseo310@gmail.com>
2024-04-06 16:13:14 +0900

    내정보

--- owner/src/app/my/page.js
+++ owner/src/app/my/page.js
@@ -3,74 +3,30 @@ import axios from "axios";
 import common from "@/resources/common.module.css";
 import Input from "@/app/component/Input";
 import Image from "next/image";
-import "/front-end/react_yanode_where_FE/owner/src/resources/my.css";
+import mycss from "@/resources/my.module.css";
 
 export default function my() {
   return (
     <>
       <div className={common.pageTitleWrap}>
-        <h2>가게 정보</h2>
+        <h2 className={mycss.pagetitle}>내 정보</h2>
         <button className={common.pageBtn}>수정</button>
       </div>
 
-      <div
-        className={common.storeContent}
-        style={{
-          display: "flex",
-          justifyContent: "space-evenly",
-          flexDirection: "column",
-          marginTop: 0,
-        }}
-      >
-        <div
-          className={common.storeLayout}
-          style={{ alignItems: "first baseline", justifyContent: "center" }}
-        >
-          <div className={common.storeImg}>
+      <div className={mycss.storeContent}>
+        <div className={mycss.storeLayout}>
+          <div className={mycss.storeImg}>
             {/*<input type="file"/>*/}
             <Image src={""} alt={"가게 사진"} />
           </div>
-          <div
-            className={common.storeInptWrap}
-            style={{ gap: "30px", width: "400px" }}
-          >
-            <input
-              type="text"
-              className={common.inpt}
-              placeholder="이름"
-              //   style={{
-              //     backgroundColor: "",
-              //     ,
-              //     ,
-              //     ,,
+          <div className={mycss.storeInptWrap}>
+            <Input name={"이름"} type={"text"} placeholder="이름" />
 
-              //   }}
-            ></input>
-            <input
-              type="text"
-              className={common.inpt}
-              placeholder="이메일"
-            ></input>
-            <input
-              type="text"
-              className={common.inpt}
-              placeholder="비밀번호"
-            ></input>
-            <input
-              type="text"
-              className={common.inpt}
-              placeholder="전화번호"
-            ></input>
-            <input
-              type="text"
-              className={common.inpt}
-              placeholder={"기본 주소"}
-            />
-            <input
-              type="text"
-              className={common.inpt}
-              placeholder={"상세 주소"}
-            />
+            <Input name={"이메일"} type={"email"} placeholder="이메일"></Input>
+            <Input name={"비밀번호"} type={"password"} placeholder="비밀번호" />
+            <Input name={"전화번호"} type={"tel"} placeholder="전화번호" />
+            <Input name={"기본주소"} type={"text"} placeholder="기본주소" />
+            <Input name={"상세주소"} type={"password"} placeholder="상세주소" />
           </div>
         </div>
       </div>
